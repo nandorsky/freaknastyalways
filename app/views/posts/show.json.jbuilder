99818a009b3b24da12d1260885494957fefeace3
Nate Andorsky <nandorsky@gmail.com>
2016-03-20 15:05:59 -0400

    creating post functionality

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :url, :description, :type, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320185127) do
+ActiveRecord::Schema.define(version: 20160320185718) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.string   "description"
+    t.integer  "type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
